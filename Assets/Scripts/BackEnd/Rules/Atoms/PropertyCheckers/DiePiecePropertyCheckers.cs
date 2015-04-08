007dbcabe12ddc9821f36fdc40363ea6a3f5feb7
RaphByrne <raph.byrne@gmail.com>
2015-04-08 23:13:47 +0800

    not everything came through

---
+++ Assets/Scripts/BackEnd/Rules/Atoms/PropertyCheckers/DiePiecePropertyCheckers.cs
@@ -0,0 +1,14 @@
+public static class DiePiecePropertyCheckers
+{
+
+	public static bool IsOne(DiePiece piece)
+	{
+		return piece.face == DiePiece.Face.One;
+	}
+
+	public static bool IsWhite(DiePiece piece)
+	{
+		return piece.colour == DiePiece.Colour.White;
+	}
+
+}
